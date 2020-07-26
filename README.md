c9edf22c1d8cf328e3731b83b0eea4f56a54e02c
Chris Atkin <atkinchris@gmail.com>
2020-07-26 16:47:04 +0100

    Add initial script and README

---
+++ README.md
@@ -0,0 +1,20 @@
+# Reddit User Image Scraper
+
+Scrapes Reddit user submissions for their main images
+
+## Usage
+
+Requires Node 10+.
+
+Put an array of usernames in `users.json`.
+
+```json
+["user1", "user2", "user3"]
+```
+
+Run the following commands. Images will appear in `images` directory.
+
+```sh
+npm install
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,69 @@
+const fetch = require('node-fetch')
+const fs = require('fs')
+const path = require('path')
+const util = require('util')
+const streamPipeline = util.promisify(require('stream').pipeline)
+
+const users = require('./users.json')
+
+const headers = {
+  'User-Agent': 'node:scraper:0.1.0 (by /u/atkinchris)',
+}
+
+const fetchWithHeaders = async (url, options = {}) => {
+  const response = await fetch(url, { ...options, headers: { ...headers, ...options.headers } })
+  if (!response.ok) throw Error(response.statusText)
+  return response
+}
+
+const fetchUserPosts = async (username) => {
+  const response = await fetchWithHeaders(`https://reddit.com/u/${username}/submitted.json`)
+  const json = await response.json()
+  return json.data.children.map((post) => post.data)
+}
+
+const postsToImages = (posts) =>
+  posts.map(({ url, created_utc, author, subreddit }) => ({
+    created: new Date(created_utc * 1000),
+    url,
+    author,
+    subreddit,
+  }))
+
+const saveImages = (images, outputDir) =>
+  Promise.all(
+    images.map(async ({ url, created, author, subreddit }) => {
+      const response = await fetchWithHeaders(url)
+      const filename = path.join(outputDir, `${subreddit}_${created}.png`)
+      await streamPipeline(response.body, fs.createWriteStream(filename))
+    })
+  )
+
+const fetchUsersImages = async (username, outputDir) => {
+  const posts = await fetchUserPosts(username)
+  const images = postsToImages(posts)
+
+  if (!images.length) return
+
+  const userOutputDir = path.join(outputDir, username)
+
+  if (!fs.existsSync(userOutputDir)) {
+    fs.mkdirSync(userOutputDir)
+  }
+
+  await saveImages(images, userOutputDir)
+}
+
+const run = async (usernames) => {
+  const outputDir = path.join(__dirname, 'images')
+
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir)
+  }
+
+  for (const username of usernames) {
+    await fetchUsersImages(username, outputDir)
+  }
+}
+
+run(['screamrocket']).catch(console.error)
